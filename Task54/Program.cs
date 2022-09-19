6f3ea40005d50e4c9484a0abdb41df4c58b1e99d
BoginskiyVV <vov4ik84@gmail.com>
2022-09-19 05:28:38 +0300

    Задача № 54

---
+++ Task54/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 54 
+// Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+FillArray(array);
+SortArray(array);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Convert.ToInt32(new Random().Next(0, 10));
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "\t  ");
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array)
+{
+    Console.WriteLine("Обработанный массив: ");
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int z = 0; z < array.GetLength(1) - 1; z++)
+            {
+                if (array[i, z] < array[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+                {
+                    int temp = 0;
+                    temp = array[i, z];
+                    array[i, z] = array[i, z + 1];
+                    array[i, z + 1] = temp;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "\t  ");
+        Console.WriteLine();
+    }
+}
